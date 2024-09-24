d68a472de6939a06f0719930b2dda8b33bda09db
Quentin Lemoine <quentin.lemoine62580@gmail.com>
2024-09-24 18:10:23 +0200

    feat(twig): remove twig

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
